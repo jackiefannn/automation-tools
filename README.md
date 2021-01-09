a7738b4e160c07a9041ddb20b0ce10b1c34d12d0
Jackie Fan <jackiefa@usc.edu>
2021-01-08 23:51:21 -0800

    Add Tracking Orders File

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# automation-tools
-This project is made to automate common tasks that need to be performed in companies. 
+# Automation Tools Using Web Scraping
+This project is made to automate common tasks that need to be performed in companies (mainly e-commerce businesses).
+
+## Tracking Orders
+Instead of manually checking to see whether or not each order has been shipped to customers 
+or not, the script will automate this task. It will **scrape through package 
+tracking sites and make requests to their APIs to track the status of the packages**.  
+
+
+**NOTE:** The script may have trouble updating the Excel database sheet if it is open by you or any others you have
+shared the Excel sheet with while the script is running. To make sure the script successfully updates the Excel sheet, 
+please make sure no one has the Excel sheet you are updating open while running the script.
+
+## Customer Info
+This script will automate the task of storing customer information on websites into an Excel database. It will 
+**take existing customer data on online platforms (in this case, Shopify) and populate the correct columns in the Excel 
+database**.
---
+++ tracking_orders.py
@@ -0,0 +1,100 @@
+import requests
+import os
+from openpyxl import load_workbook
+from openpyxl.styles import Font, Alignment
+import time
+import datetime
+from datetime import date
+
+
+
+def trackingIdDecryption(trackingNumber):
+    trackingIdEncryption = {'A': 'S', 'B': 'T', 'C': 'U', 'D': 'V', 'E': 'W', 'F': 'X', 'G': 'Y', 'H': 'Z', 'I': '%5B',
+                            'J': '%5C', 'K': '%5D', 'L': '%5E', 'M': '_', 'N': '%60', 'O': 'a', 'P': 'b', 'Q': 'c',
+                            'R': 'd', 'S': 'e', 'T': 'f', 'U': 'g', 'V': 'h', 'W': 'i', 'X': 'j', 'Y': 'k', 'Z': 'l',
+                            '0': 'B', '1': 'C', '2': 'D', '3': 'E', '4': 'F', '5': 'G', '6': 'H', '7': 'I',
+                            '8': 'J', '9': 'K'}
+    # print([trackingIdEncryption[letter] for letter in trackingNumber])
+    # print('Parcel trackingId: ' + "".join([trackingIdEncryption[letter] for letter in trackingNumber]))
+    return "".join([trackingIdEncryption[letter] for letter in trackingNumber])
+
+def readExcelFile():
+    # get handle on existing file
+    wb = load_workbook(filename=os.environ['EXCEL_SHEET'])
+    # get current month and year worksheet
+    month_year = date.today().strftime("%b-%Y").split("-")
+    worksheet = "{month}. {year}".format(month=month_year[0], year=month_year[1])
+    # print(worksheet)
+    ws = wb[worksheet]
+    font = Font(name='Times New Roman', size=12)
+    alignment = Alignment(horizontal='center')
+    # get columns
+    shipping_status_column = 'M'
+    time_column = 'N'
+    tracking_number_column = 'O'
+    # loop through range values
+    for i in range(81, 91):
+        cell = tracking_number_column + str(i)
+        if isinstance(ws[cell].value, str):
+            # track only orders that have not been delivered yet
+            if ws[tracking_number_column + str(i)].value != 'Delivered':
+                print('Tracking Number: ' + ws[cell].value)
+                status = getShippingStatus(ws[cell].value)
+                ws[shipping_status_column + str(i)] = status
+                ws[time_column + str(i)] = datetime.datetime.now()
+        else:
+            ws[shipping_status_column + str(i)] = 'No Tracking Number'
+            ws[shipping_status_column + str(i)] = 'No Tracking Number'
+        ws[shipping_status_column + str(i)].font = font
+        ws[shipping_status_column + str(i)].alignment = alignment
+        ws[time_column + str(i)].font = font
+        ws[time_column + str(i)].alignment = alignment
+        print(ws[shipping_status_column + str(i)].value)
+        print(ws[time_column + str(i)].value)
+
+    wb.save(filename=os.environ['EXCEL_SHEET'])
+
+
+def getShippingStatus(trackingNum):
+    request_url = os.environ['TRACKING_API']
+    url = os.environ['TRACKING_SITE'] + trackingNum
+    trackingId = trackingIdDecryption(trackingNum)
+    data = {
+        'trackingId': trackingId,
+        'carrier': 'Auto-Detect',
+        'language': 'en',
+        'country': 'Russian Federation',
+        'platform': 'web-desktop',
+        'wd': 'false',
+        'c': 'false',
+        'p': 3,
+        'l': 2,
+        'se': '1792x1024,MacIntel,Gecko,Mozilla,Netscape,Google Inc.,4g,Intel Inc.,Intel(R) UHD Graphics 630,undefined,103,3584,2048'}
+
+    resp = requests.post(request_url, data=data)
+    print(resp)
+    try:
+        print(resp.json())
+        if 'error' in resp.json():
+            print('Error: ' + resp.json()['error'].title())
+            return resp.json()['error'].title()
+        elif 'sub_status' in resp.json():
+            print('Sub-status: ' + resp.json()['sub_status'].title())
+            return resp.json()['sub_status'].title()
+        else:
+            print('Status: ' + resp.json()['status'].title())
+            return resp.json()['status'].title()
+    # check to see if request does not give back a valid json
+    except ValueError:
+        print('The request did not return any JSON')
+        return 'Too many requests'
+
+
+if __name__ == '__main__':
+    start = time.time()
+    readExcelFile()
+    end = time.time()
+    # total time taken
+    print(f"Runtime of the program is {end - start}")
+
+
