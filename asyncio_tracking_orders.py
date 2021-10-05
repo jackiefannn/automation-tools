7468a4ae6f3618c0cbc20f4a32427dc7b1fe6947
Jackie Fan <jackiefa@usc.edu>
2021-10-04 22:26:09 -0700

    updated tracking id

--- asyncio_tracking_orders.py
+++ asyncio_tracking_orders.py
@@ -10,32 +10,7 @@ import os
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Alignment
 
-url = os.environ['TRACKING_API']
-database = os.environ['EXCEL_SHEET']
-# get handle on existing file
-wb = load_workbook(filename=os.environ['EXCEL_SHEET'])
-# get current month and year worksheet
-month_year = date.today().strftime("%b-%Y").split("-")
-worksheet = "{month}. {year}".format(month=month_year[0], year=month_year[1])
-ws = wb[worksheet]
-font = Font(name='Times New Roman', size=12)
-alignment = Alignment(horizontal='center')
-row = 2
-sale_order_col = 'A'
-shipping_status_col = 'N'
-time_col = 'O'
-tracking_number_col = 'P'
-
-def trackingIdDecryption(trackingNumber):
-    trackingIdEncryption = {'A': 'S', 'B': 'T', 'C': 'U', 'D': 'V', 'E': 'W', 'F': 'X', 'G': 'Y', 'H': 'Z',
-                            'I': '%5B',
-                            'J': '%5C', 'K': '%5D', 'L': '%5E', 'M': '_', 'N': '%60', 'O': 'a', 'P': 'b', 'Q': 'c',
-                            'R': 'd', 'S': 'e', 'T': 'f', 'U': 'g', 'V': 'h', 'W': 'i', 'X': 'j', 'Y': 'k',
-                            'Z': 'l',
-                            '0': 'B', '1': 'C', '2': 'D', '3': 'E', '4': 'F', '5': 'G', '6': 'H', '7': 'I',
-                            '8': 'J', '9': 'K'}
-    return "".join([trackingIdEncryption[letter] for letter in trackingNumber])
-
+# initialize basic logging information for us to use
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
     level=logging.DEBUG,
@@ -46,18 +21,32 @@ logger = logging.getLogger("areq")
 logging.getLogger("chardet.charsetprober").disabled = True
 
 
+def trackingIdDecryption(trackingNumber):
+    trackingIdEncryption = {'A': '%01', 'B': '%02', 'C': '%03', 'D': '%04', 'E': '%05', 'F': '%06', 'G': '%07',
+                            'H': '%08', 'I': '%09', 'J': '%0A', 'K': '%0B', 'L': '%0C', 'M': '%0D', 'N': '%0E',
+                            'O': '%0F', 'P': '%10', 'Q': '%11', 'R': '%12', 'S': '%13', 'T': '%14', 'U': '%15',
+                            'V': '%16', 'W': '%17', 'X': '%18', 'Y': '%19', 'Z': '%1A',
+                            '0': 'n', '1': 'o', '2': 'p', '3': 'q', '4': 'r', '5': 's', '6': 't', '7': 'u',
+                            '8': 'v', '9': 'w'}
+    return "".join([trackingIdEncryption[letter] for letter in trackingNumber])
+
+
 async def fetch_html(url: str, session: ClientSession, data, **kwargs) -> str:
     """
     GET request wrapper to fetch page HTML.
     kwargs are passed to `session.request()`.
     """
-
-    resp = await session.request(method="POST", url=url, data=data, **kwargs)
-    resp.raise_for_status()
-    logger.info("Got response [%s] for URL: %s", resp.status, url)
-    doc = await resp.json()
-    logger.info("JSON Response of request: %s", doc)
-    return doc
+    headers = {'content_type': 'text/html', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'}
+    try:
+        resp = await session.request(method="POST", url=url, data=data, headers=headers, **kwargs)
+        resp.raise_for_status()
+        logger.info("Got response [%s] for URL: %s", resp.status, url)
+        doc = await resp.json(content_type=None)
+        logger.info("JSON Response of request: %s", doc)
+    except Exception as ve:
+        logger.error("Too many requests, the request did not return any JSON and returned nothing: %s", getattr(ve, "__dict__", {}))
+    else:
+        return doc
 
 
 async def fetch_page(url: str, session: ClientSession, data, **kwargs):
@@ -78,8 +67,6 @@ async def fetch_and_write_one(url: str, database, session, data, row, **kwargs):
     """ Fetch JSON doc and return status of shipping order """
     doc = await fetch_page(url=url, session=session, data=data, **kwargs)
     if not doc: return None
-    # if len(doc) == 0:
-    #     return None
     # logger.info(f"Contents of the JSON response: {doc}")
     status = str
     if 'error' in doc:
@@ -106,11 +93,12 @@ async def fetch_and_write(url: str, database, **kwargs) -> None:
             # track sales orders that have a shipping number
             if isinstance(ws[f"{tracking_number_col}{row}"].value, str):
                 # track only orders that have not been delivered yet
-                if ws[f"{tracking_number_col}{row}"].value != 'Delivered' or \
-                        ws[f"{tracking_number_col}{row}"].value != 'Arrived' or \
-                        ws[f"{tracking_number_col}{row}"].value !='Pickup':
+                if ws[f"{shipping_status_col}{row}"].value != 'Delivered' and \
+                        ws[f"{shipping_status_col}{row}"].value != 'Arrived' and \
+                        ws[f"{shipping_status_col}{row}"].value != 'Pickup':
                     trackingNum = ws[f"{tracking_number_col}{row}"].value
                     logger.info(f"Row: {row} & Tracking num: {trackingNum}")
+                    logger.info(f"Current Status: {ws[shipping_status_col + str(row)].value}")
                     trackingId = trackingIdDecryption(trackingNum)
                     data = {
                         'trackingId': trackingId,
@@ -127,8 +115,13 @@ async def fetch_and_write(url: str, database, **kwargs) -> None:
                     res = fetch_and_write_one(url=url, database=database, session=session, data=data, row=row, **kwargs)
                     # if not res: continue
                     post_tasks.append(res)
+
+                    # execute x number of async requests at a time (no need to execute them all at the same time)
+                    if len(post_tasks) > 50:
+                        await asyncio.gather(*post_tasks)
+                        post_tasks = []
             row += 1
-        # now execute them all at once
+        # now execute the rest of the remaining requests
         await asyncio.gather(*post_tasks)
 
 
@@ -151,6 +144,21 @@ async def as_insert_one(database, status, row):
 if __name__ == '__main__':
     start = time.time()
     # total time taken
+    url = os.environ['TRACKING_API']
+    database = os.environ['EXCEL_SHEET']
+    # get handle on existing file
+    wb = load_workbook(filename=os.environ['EXCEL_SHEET'])
+    # get current month and year worksheet
+    month_year = date.today().strftime("%b-%Y").split("-")
+    worksheet = "{month}. {year}".format(month=month_year[0], year=month_year[1])
+    ws = wb[worksheet]
+    font = Font(name='Times New Roman', size=12)
+    alignment = Alignment(horizontal='center')
+    row = 2
+    sale_order_col = 'A'
+    shipping_status_col = 'N'
+    time_col = 'O'
+    tracking_number_col = 'P'
     asyncio.run(fetch_and_write(url=url, database=database))
     end = time.time()
     logger.info(f"Runtime of the program is {end - start}")
--- tracking_orders.py
+++ tracking_orders.py
@@ -7,13 +7,13 @@ import datetime
 from datetime import date
 
 
-
 def trackingIdDecryption(trackingNumber):
-    trackingIdEncryption = {'A': 'S', 'B': 'T', 'C': 'U', 'D': 'V', 'E': 'W', 'F': 'X', 'G': 'Y', 'H': 'Z', 'I': '%5B',
-                            'J': '%5C', 'K': '%5D', 'L': '%5E', 'M': '_', 'N': '%60', 'O': 'a', 'P': 'b', 'Q': 'c',
-                            'R': 'd', 'S': 'e', 'T': 'f', 'U': 'g', 'V': 'h', 'W': 'i', 'X': 'j', 'Y': 'k', 'Z': 'l',
-                            '0': 'B', '1': 'C', '2': 'D', '3': 'E', '4': 'F', '5': 'G', '6': 'H', '7': 'I',
-                            '8': 'J', '9': 'K'}
+    trackingIdEncryption = {'A': '%01', 'B': '%02', 'C': '%03', 'D': '%04', 'E': '%05', 'F': '%06', 'G': '%07',
+                            'H': '%08', 'I': '%09', 'J': '%0A', 'K': '%0B', 'L': '%0C', 'M': '%0D', 'N': '%0E',
+                            'O': '%0F', 'P': '%10', 'Q': '%11', 'R': '%12', 'S': '%13', 'T': '%14', 'U': '%15',
+                            'V': '%16', 'W': '%17', 'X': '%18', 'Y': '%19', 'Z': '%1A',
+                            '0': 'n', '1': 'o', '2': 'p', '3': 'q', '4': 'r', '5': 's', '6': 't', '7': 'u',
+                            '8': 'v', '9': 'w'}
     return "".join([trackingIdEncryption[letter] for letter in trackingNumber])
 
 def readExcelFile(startRow, endRow):
@@ -21,8 +21,7 @@ def readExcelFile(startRow, endRow):
     wb = load_workbook(filename=os.environ['EXCEL_SHEET'])
     # get current month and year worksheet
     month_year = date.today().strftime("%b-%Y").split("-")
-    # worksheet = "{month}. {year}".format(month=month_year[0], year=month_year[1])
-    worksheet = 'Jan. 2021'
+    worksheet = "{month}. {year}".format(month=month_year[0], year=month_year[1])
     # print(worksheet)
     ws = wb[worksheet]
     font = Font(name='Times New Roman', size=12)
@@ -41,7 +40,7 @@ def readExcelFile(startRow, endRow):
         cell = tracking_number_col + str(i)
         if isinstance(ws[cell].value, str):
             # track only orders that have not been delivered yet
-            if ws[tracking_number_col + str(i)].value != 'Delivered':
+            if ws[shipping_status_col + str(i)].value != 'Delivered':
                 print('Row ' + str(i) + ' Tracking Number: ' + ws[cell].value)
                 status = getShippingStatus(ws[cell].value)
                 if status is not None: ws[shipping_status_col + str(i)] = status
@@ -62,8 +61,9 @@ def readExcelFile(startRow, endRow):
 
 def getShippingStatus(trackingNum):
     request_url = os.environ['TRACKING_API']
-    url = os.environ['TRACKING_SITE'] + trackingNum
+    url = f"{os.environ['TRACKING_SITE']}{trackingNum}"
     trackingId = trackingIdDecryption(trackingNum)
+    print(trackingId)
     data = {
         'trackingId': trackingId,
         'carrier': 'Auto-Detect',
@@ -74,10 +74,10 @@ def getShippingStatus(trackingNum):
         'c': 'false',
         'p': 3,
         'l': 2,
-        'se': '1792x1024,MacIntel,Gecko,Mozilla,Netscape,Google Inc.,4g,Intel Inc.,Intel(R) UHD Graphics 630,undefined,103,3584,2048'}
-
+        'se': '1792x1017,MacIntel,Gecko,Mozilla,Netscape,Google Inc.,4g,Intel Inc.,Intel(R) UHD Graphics 630,undefined,103,3584,2034'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
     try:
-        resp = requests.post(request_url, data=data)
+        resp = requests.post(request_url, data=data, headers=headers)
         print(resp)
         print(resp.json())
         if 'error' in resp.json():
@@ -100,10 +100,11 @@ def getShippingStatus(trackingNum):
 
 if __name__ == '__main__':
     try:
-        startRow = int(input('Please input the first row you would like to track: '))
-        endRow = int(input('Please input the last row you would like to track up to: '))
-        # startRow = 501
-        # endRow = 550
+        # startRow = int(input('Please input the first row you would like to track: '))
+        # endRow = int(input('Please input the last row you would like to track up to: '))
+        startRow = 2
+        endRow = 4
+        # last row is 2247
         start = time.time()
         readExcelFile(startRow, endRow)
         end = time.time()
@@ -112,5 +113,3 @@ if __name__ == '__main__':
     except ValueError as ve:
         print('Error Message: ' + str(ve))
         print('Please input valid integers for the start and end rows.')
-
-
